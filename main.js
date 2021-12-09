b24dd7073968be9ee5c75124c4e185ad7f84e96e
Vito9009 <yan.vi193@gmail.com>
2021-12-09 16:36:40 +0100

    chiusura-menu

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-const openHamburgerMenu = document.querySelector(".fa-bars");
+const openHamburgerMenu = document.querySelector(".fas.fa-bars");
+
 openHamburgerMenu.addEventListener("click", function(){
     const element = document.querySelector(".hamburger-menu");
     element.classList.add("active");
+});
+
+const closeHamburgerMenu = document.querySelector(".fas.fa-times");
+
+closeHamburgerMenu.addEventListener("click", function(){
+    const element = document.querySelector(".hamburger-menu");
+    element.classList.remove("active");
 });
